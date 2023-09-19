81c6b45301d544538e10b63ff7341531fe15916b
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-19 10:30:09 -0300

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,18 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args){
 
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(21, "caio", "caio@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 
         System.out.println(obj);
     }
