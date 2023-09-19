a66d72052f392776a346ea471748be28c95a3a35
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-19 11:28:15 -0300

    findById implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -11,13 +11,13 @@ public class Program {
 
     public static void main(String[] args){
 
-        Department obj = new Department(1, "books");
-        Seller seller = new Seller(21, "caio", "caio@gmail.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
 
-        System.out.println(obj);
+
     }
 }
