87c666fffac6293e90c36c61895d560be22d6331
CaioVSRodrigues <caiovsrodriguess@gmail.com>
2023-09-19 15:27:29 -0300

    findAll implementation

--- src/app/Program2.java
+++ src/app/Program2.java
@@ -4,6 +4,7 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Program2 {
@@ -18,12 +19,18 @@ public class Program2 {
         Department dep = departmentDao.findById(1);
         System.out.println(dep);
 
-       System.out.println("\n=== TEST 1: insert: ===");
+        System.out.println("\n=== TEST 2: findById =======");
+        List<Department> list = departmentDao.findAll();
+            for(Department d : list){
+                System.out.println(d);
+            }
+
+       System.out.println("\n=== TEST 3: insert: ===");
        Department newdepartment = new Department(null, "Dance");
        departmentDao.insert(newdepartment);
        System.out.println("Inserted! New id = " + newdepartment.getId());
 
-        System.out.println("\n=== TEST 3: update =======");
+        System.out.println("\n=== TEST 4: update =======");
         Department dep2 = departmentDao.findById(1);
         dep2.setName("Food");
         departmentDao.update(dep2);
